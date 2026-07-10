0cd71f3fac2e5f7b7932520fc8def4e33a2bbd16
vivek-mistry <vivek@weblog-media.com>
2026-07-10 22:38:27 +0530

    Fixed routes

--- routes/web.php
+++ routes/web.php
@@ -7,16 +7,13 @@ use App\Http\Controllers\StudentController;
 use App\Http\Middleware\EnsureStudentIsAuthenticated;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+// Route::get('/', function () {
+//     return view('welcome');
+// });
 
-Route::get('/readme', [ReadmeController::class, 'index'])->name('readme');
+Route::get('/', [ReadmeController::class, 'index'])->name('readme');
 
-// Guest routes (login / register)
-Route::get('/login', [LoginController::class, 'login'])->name('login');
-Route::post('/authenticate', [LoginController::class, 'authCheck'])->name('authenticate');
-Route::post('/logout', [LoginController::class, 'destroy'])->name('logout');
+
 
 Route::get('/register', [RegisterController::class, 'create'])->name('students.register');
 Route::post('/register', [RegisterController::class, 'store'])->name('students.store');
@@ -28,3 +25,9 @@ Route::get('/students/{student}/edit', [StudentController::class, 'edit'])->name
 Route::put('/students/{student}', [StudentController::class, 'update'])->name('students.update');
 Route::delete('/students/{student}', [StudentController::class, 'destroy'])->name('students.destroy');
 // });
+
+
+// Guest routes (login / register)
+Route::get('/login', [LoginController::class, 'login'])->name('login');
+Route::post('/authenticate', [LoginController::class, 'authCheck'])->name('authenticate');
+Route::post('/logout', [LoginController::class, 'destroy'])->name('logout');
