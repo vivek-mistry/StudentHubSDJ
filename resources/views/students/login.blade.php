2dd41b894def7cdc8a9ac4ec95dab0c86e0bd3a5
vivek-mistry <vivek@weblog-media.com>
2026-07-10 18:48:26 +0530

    update : bootstrap with locally could run the project ✅

--- resources/views/students/login.blade.php
+++ resources/views/students/login.blade.php
@@ -6,7 +6,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Laravel Unlocked - Login</title>
 
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
+    @vite(['resources/css/app.css', 'resources/js/app.js'])
 
     <style>
         body {
